b6ed868a0f5dd69397b1ab53ac01e332a86bbaea
Masdo <Masteqipoor@gmail.com>
2021-08-15 19:31:11 +0430

    database Cxnn is completed Connection

--- src/main/scala/OracleDBConnection/oracleDBConnection.scala
+++ src/main/scala/OracleDBConnection/oracleDBConnection.scala
@@ -1,32 +1,81 @@
 package OracleDBConnection
 
-import java.sql.DriverManager
+import java.sql.{CallableStatement, DriverManager, ResultSet, Statement, Types}
 
-object oracleDBConnection extends App {
-      // connect to the database named "mysql" on the localhost
-      val driver = "oracle.jdbc.OracleDriver"
-      val url = "jdbc:oracle:thin:@192.168.122.1:1521:orclcdb"
-      val username = "testDB"
-      val password = "Ma1234"
-      // there's probably a better way to do this
-      var connection = DriverManager.getConnection(url, username, password)
-      try {
-        // make the connection
-        Class.forName(driver)
-        // create the statement, and run the select query
-        val statement = connection.createStatement()
-        val terminalkey = "000096411017"
-        val pspkey = "1005"
-        val query = s"select * from terminals_tbl where terminalkey= '$terminalkey'"
-        val resultSet = statement.executeQuery(query)
-        while ( resultSet.next() )
+
+object oracleDBConnection {
+  final val driver = "oracle.jdbc.OracleDriver"
+  final val url = "jdbc:oracle:thin:@192.168.122.1:1521:orclcdb"
+  final val username = "testDB"
+  final val password = "Ma1234"
+  final val connection = DriverManager.getConnection(url, username, password)
+  case class total_Terminal (terminal_key:String , guild_code:String , guild_desc:String , mega_guild_desc:String)
+  case class terminal(terminalKey:String , guild_Code:String)
+}
+
+class oracleDBConnection
+{
+  import OracleDBConnection.oracleDBConnection._
+  def getResults(resultSet: ResultSet , resultList: List[terminal] = List()):
+      List[terminal] =
+      {
+        if (resultSet.next() == true)
         {
-          val guild = resultSet.getString("trmrchtyp")
-          val pspkey = resultSet.getString("pspkey")
-          println("(guild,pspkey) = (" + guild + ", " + pspkey + ")")
+          val guildCode = resultSet.getString("trmrchtyp")
+          val TerminalKey = resultSet.getString("terminalkey")
+          val rltLst = resultList :+ terminal(terminalKey = TerminalKey , guild_Code = guildCode)
+          getResults(resultSet, rltLst)
         }
-      } catch {
-        case e => e.printStackTrace
+        else {
+          resultSet.close()
+          resultList
+        }
       }
+
+  def execute_sql_query(terminalKeyList:List[String]): List[terminal] =
+    {
+      val terminal_string = terminalKeyList.mkString("','")
+      val query = s"select trmrchtyp, terminalkey from terminals_tbl where terminalkey in ('$terminal_string')"
+      val resultSet = connection.createStatement().executeQuery(query)
+      val results = getResults(resultSet)
       connection.close()
+      results
+    }
+
+  def createCallableStatement(ProcName:String , number_of_elements:Int , InputList : List[String]) : CallableStatement =
+    {
+      val l = InputList.length
+      val q = List.fill(number_of_elements)("?")
+      val sql = s"{call $ProcName(${q.mkString(",")})}"
+      val callableStatement = connection.prepareCall(sql)
+      for (i <- 1 to l)
+      {
+        callableStatement.setString(i, InputList(i-1))
+      }
+      for (j <- l + 1 to number_of_elements)
+      {
+        callableStatement.registerOutParameter(j , Types.VARCHAR)
+      }
+      callableStatement.executeUpdate()
+      callableStatement
+    }
+
+  def get_terminal_info(terminalID:String,PSPKey:String):total_Terminal = {
+    val qu = createCallableStatement("getTerminalGuild", 6 , List(terminalID,PSPKey))
+    val guild_code = qu.getString(3)
+    val guild_desc = qu.getString(4)
+    val mega_guild_desc = qu.getString(5)
+    val TerminalKey = qu.getString(6)
+    total_Terminal(TerminalKey, guild_code, guild_desc, mega_guild_desc)
+  }
+
+  //try {
+  //      // make the connection
+  //      Class.forName(driver)
+  //      // create the statement, and run the select query
+  //    } catch {
+  //      case e => e.printStackTrace
+  //    }
+  //    connection.close()
+
 }
--- src/main/scala/ServerSideAPI/PFM_Low_level_API.scala
+++ src/main/scala/ServerSideAPI/PFM_Low_level_API.scala
@@ -1,149 +1,143 @@
 package ServerSideAPI
-
+/* import asynchronized request Handler */
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.IncomingConnection
 import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink}
-
 import scala.concurrent.Future
 
+
+import OracleDBConnection.{oracleDBConnection}
+
 object PFM_Low_level_API extends App {
-  println("ready to start masoud lets go")
+  val V:oracleDBConnection = new oracleDBConnection
+  val T = V.execute_sql_query(List("000096411017", "000096461008", "000097211017"))
+  println(T)
 
-  implicit val system = ActorSystem("LowLevelServerAPI")
-  implicit val materializer = ActorMaterializer()
-  import system.dispatcher
-
-  /*val serverSource = Http().bind("localhost" , 8080)*/
-  val connectionSink = Sink.foreach[IncomingConnection]{
-    connection => println(s"Accepted Incoming connection from:${connection.remoteAddress}")
-  }
-//  val serverBindingFuture = serverSource.to(connectionSink).run()
-//  serverBindingFuture.onComplete
-//  {
-//    case Success(binding) =>
-//      println("successful binding")
-//      binding.terminate(2 seconds)
-//    case Failure(ex) => println(s"failed with code : $ex")
+  //  println("ready to start masoud lets go")
+//  implicit val system = ActorSystem("PFMLowLevelAPI")
+//  implicit val materializer = ActorMaterializer()
+//  import system.dispatcher
+//
+//  /*val serverSource = Http().bind("localhost" , 8080)*/
+//  val connectionSink = Sink.foreach[IncomingConnection]
+//    {
+//      connection => println(s"Accepted Incoming connection from:${connection.remoteAddress}")
+//    }
+////  val serverBindingFuture = serverSource.to(connectionSink).run()
+////  serverBindingFuture.onComplete
+////  {
+////    case Success(binding) =>
+////      println("successful binding")
+////      binding.terminate(2 seconds)
+////    case Failure(ex) => println(s"failed with code : $ex")
+////  }
+//
+//  val requestHandler:HttpRequest => HttpResponse = {
+//    case HttpRequest(HttpMethods.GET, _, _, _, _) =>
+//      HttpResponse(
+//        StatusCodes.OK, //Http 200
+//        entity = HttpEntity(
+//          ContentTypes.`text/html(UTF-8)`,
+//        """
+//          |<html>
+//          | <body>
+//          | Oops I am going to be great you Bet!!!
+//          | </body>
+//          |</html>
+//        """.stripMargin
+//        )
+//        )
+//    case request: HttpRequest => request.discardEntityBytes()
+//      HttpResponse(
+//        StatusCodes.NotFound, // error 404
+//        entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+//      """
+//        |<html>
+//        | <body>
+//        | Oops No terminal Found!!!
+//        | </body>
+//        |</html>
+//      """.stripMargin
+//    )
+//    )
 //  }
-
-  val requestHandler:HttpRequest => HttpResponse = {
-    case HttpRequest(HttpMethods.GET, _, _, _, _) =>
-      HttpResponse(
-        StatusCodes.OK, //Http 200
-        entity = HttpEntity(
-          ContentTypes.`text/html(UTF-8)`,
-        """
-          |<html>
-          | <body>
-          | Oops I am going to be great you Bet!!!
-          | </body>
-          |</html>
-        """.stripMargin
-        )
-        )
-    case request: HttpRequest => request.discardEntityBytes()
-      HttpResponse(
-        StatusCodes.NotFound, // error 404
-        entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
-      """
-        |<html>
-        | <body>
-        | Oops Akka says Hello!!!
-        | </body>
-        |</html>
-      """.stripMargin
-    )
-    )
-  }
-
-  val httpSyncConnectionHandler = Sink.foreach[IncomingConnection]{
-    connection => connection.handleWithSyncHandler(requestHandler)
-  }
-//  Http().bind("localhost",8080).runWith(httpSyncConnectionHandler)
-
-  val asyncRequestHandler:HttpRequest => Future[HttpResponse] = {
-    case HttpRequest(HttpMethods.GET, Uri.Path("/home"), _, _, _) =>
-      Future(HttpResponse(
-        StatusCodes.OK, //Http 200
-        entity = HttpEntity(
-          ContentTypes.`text/html(UTF-8)`,
-          """
-            |<html>
-            | <body>
-            | Oops I am going to be great you Bet!!!
-            | </body>
-            |</html>
-        """.stripMargin
-        )
-      )
-    )
-    case request: HttpRequest => request.discardEntityBytes()
-      Future(HttpResponse(
-        StatusCodes.NotFound, // error 404
-        entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
-          """
-            |<html>
-            | <body>
-            | Oops Akka says Hello!!!
-            | </body>
-            |</html>
-          """.stripMargin
-       )
-      )
-    )
-  }
-  val httpAsyncConnectionHandler = Sink.foreach[IncomingConnection]{
-    connection => connection.handleWithAsyncHandler(asyncRequestHandler)
-  }
-//  Http().bind("localhost",8080).runWith(httpAsyncConnectionHandler)
-//  Http().bindAndHandleAsync(asyncRequestHandler,"localhost",8081)
-
-  val streamsBasedRequestHandler:Flow[HttpRequest,HttpResponse, _]= Flow[HttpRequest].map{
-    case HttpRequest(HttpMethods.GET,Uri.Path("/home"),_,_,_) => HttpResponse(
-      StatusCodes.OK, // Http 200
-      entity = HttpEntity(
-        ContentTypes.`text/html(UTF-8)`,
-        """
-          |<html>
-          | <body>
-          | hello, How di, your talking to the greatest Iranian Programmer ever!!!
-          | </body>
-          |</html>
-        """.stripMargin
-      )
-    )
-
-    case request: HttpRequest => request.discardEntityBytes()
-      HttpResponse(
-        StatusCodes.NotFound, // error 404
-        entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
-          """
-            |<html>
-            | <body>
-            | Oops Akka says Hello To the greatest Akka !!!
-            | </body>
-            |</html>
-      """.stripMargin
-        )
-      )
-    }
-//  Http().bind("localhost",8082).runForeach{
-//    connection => connection.handleWith(streamsBasedRequestHandler)}
-  Http().bindAndHandle(streamsBasedRequestHandler,"localhost",8082)
-  /* curling braces and this might be the main issue */
-  /* the destination headers */
-  /* 302 found */
-
-//  val bindingFuture = Http().bindAndHandleSync(httpAsyncConnectionHandler)
-//  bindingFuture.flatMap(binding => binding.unbind()).
-//    onComplete(_ => system.terminate())
-  /* This is not what it seems to be mine */
-  /* the main issue stays the same as the problems stand*/
-  /* handel suy the mian issue would be nice from this view point */
-  /* the other */
-  /* this is only to completely fuck tou up */
-  /**/
+//
+//  val httpSyncConnectionHandler = Sink.foreach[IncomingConnection]{
+//    connection => connection.handleWithSyncHandler(requestHandler)
+//  }
+////  Http().bind("localhost",8080).runWith(httpSyncConnectionHandler)
+//
+//  val asyncRequestHandler:HttpRequest => Future[HttpResponse] = {
+//    case HttpRequest(HttpMethods.GET, Uri.Path("/home"), _, _, _) =>
+//      Future(HttpResponse(
+//        StatusCodes.OK, //Http 200
+//        entity = HttpEntity(
+//          ContentTypes.`text/html(UTF-8)`,
+//          """
+//            |<html>
+//            | <body>
+//            | Oops I am going to be great you Bet!!!
+//            | </body>
+//            |</html>
+//        """.stripMargin
+//        )
+//      )
+//    )
+//
+//    case request: HttpRequest => request.discardEntityBytes()
+//      Future(HttpResponse(
+//        StatusCodes.NotFound, // error 404
+//        entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+//          """
+//            |<html>
+//            | <body>
+//            | Oops Akka says Hello!!!
+//            | </body>
+//            |</html>
+//          """.stripMargin
+//       )
+//      )
+//    )
+//  }
+//  val httpAsyncConnectionHandler = Sink.foreach[IncomingConnection]{
+//    connection => connection.handleWithAsyncHandler(asyncRequestHandler)
+//  }
+////  Http().bind("localhost",8080).runWith(httpAsyncConnectionHandler)
+////  Http().bindAndHandleAsync(asyncRequestHandler,"localhost",8081)
+//
+//  val streamsBasedRequestHandler:Flow[HttpRequest,HttpResponse, _]= Flow[HttpRequest].map{
+//    case HttpRequest(HttpMethods.GET,Uri.Path("/home"),_,_,_) => HttpResponse(
+//      StatusCodes.OK, // Http 200
+//      entity = HttpEntity(
+//        ContentTypes.`text/html(UTF-8)`,
+//        """
+//          |<html>
+//          | <body>
+//          | hello, How di, your talking to the greatest Iranian Programmer ever!!!
+//          | </body>
+//          |</html>
+//        """.stripMargin
+//      )
+//    )
+//
+//    case request: HttpRequest => request.discardEntityBytes()
+//      HttpResponse(
+//        StatusCodes.NotFound, // error 404
+//        entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
+//          """
+//            |<html>
+//            | <body>
+//            | Oops Akka says Hello To the greatest Akka !!!
+//            | </body>
+//            |</html>
+//      """.stripMargin
+//        )
+//      )
+//    }
+////  Http().bind("localhost",8082).runForeach{
+////    connection => connection.handleWith(streamsBasedRequestHandler)}
+//  Http().bindAndHandle(streamsBasedRequestHandler,"localhost",251251)
 }
